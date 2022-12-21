44899604592c11da22c18b6655521486d44d0a25
ElaisTheFixer <106221783+ElaisTheFixer@users.noreply.github.com>
2022-12-21 13:23:02 +1000

    Add files via upload

--- contracts/YeildPrac.sol
+++ contracts/YeildPrac.sol
@@ -17,7 +17,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./ExodusStaking.sol";
+import "./YieldStaking.sol";
 
 interface IUniswapV2Factory {
     event PairCreated(
